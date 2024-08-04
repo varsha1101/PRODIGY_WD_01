ff1778d0fa9688efc62bef21cfc6c3053cd71846
varsha1101 <144980497+varsha1101@users.noreply.github.com>
2024-08-04 20:19:47 +0530

    Add files via upload

---
+++ prodigy task 1.script.js
@@ -0,0 +1,67 @@
+// Get the navigation bar
+
+var navbar = document.getElementById("navbar");
+
+
+// Get the offset position of the navbar
+
+var sticky = navbar.offsetTop;
+
+
+// Function to add scroll event listener
+
+function onScroll()
+{
+    
+if (window.pageYOffset >= sticky) 
+{
+        
+navbar.classList.add("nav-scrolled");
+
+} 
+else 
+{
+        
+navbar.classList.remove("nav-scrolled");
+   
+}
+
+}
+
+
+// Listen for scroll events
+
+window.onscroll = function() 
+{
+    
+onScroll();
+
+};
+
+
+// Add hover effects to menu items
+
+var menuItems = document.querySelectorAll("#navbar ul li");
+
+
+menuItems.forEach(function(item) 
+{
+    
+item.addEventListener("mouseenter", function() 
+{
+        
+this.querySelector("a").style.color = "#ff7f50"; 
+// Example hover color
+    
+});
+    
+    
+item.addEventListener("mouseleave", function() 
+{
+        
+this.querySelector("a").style.color = "#fff"; 
+// Revert to original color
+    
+});
+
+});
